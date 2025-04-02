6681418c832aa4a7d868f444b4fcd63e63dead30
Ouyang Kun <131441216+OuyangKun10@users.noreply.github.com>
2025-04-02 23:26:03 +0800

    Add files via upload

---
+++ src/open-r1-multimodal/src/open_r1/trainer/__init__.py
@@ -0,0 +1,4 @@
+from .grpo_trainer import Qwen2VLGRPOTrainer
+from .grpo_config import GRPOConfig
+from .grpo_trainer_modified import Qwen2VLGRPOTrainer as Qwen2VLGRPOTrainer_v
+__all__ = ["Qwen2VLGRPOTrainer","Qwen2VLGRPOTrainer_v"]
