7dca9d4616474f50af44c32f9ece5d96d81bf762
ouyangkun <ouyangkun@moonshot.cn>
2025-05-18 10:26:08 +0000

    delete previous version of codes

--- src/open-r1-multimodal/src/open_r1/trainer/__init__.py
+++
@@ -1,4 +0,0 @@
-from .grpo_trainer import Qwen2VLGRPOTrainer
-from .grpo_config import GRPOConfig
-from .grpo_trainer_modified import Qwen2VLGRPOTrainer as Qwen2VLGRPOTrainer_v
-__all__ = ["Qwen2VLGRPOTrainer","Qwen2VLGRPOTrainer_v"]
